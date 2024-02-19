54c9cdc432dee5a492283ae2889445a60b58ec74
KT-3620 <kt.app.support@icloud.com>
2024-02-19 23:22:29 +0900

    feat: サーバーアイコンの定時編集を実装

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,32 +1,22 @@
-import { cronCheck } from "./function/cronCheck";
 import * as schedule from "node-schedule";
 import { logger } from "./function/logger";
+import { Guild } from "discord.js";
+import { getRandomImage } from "./function/getRandomImage";
 
 /** スケジュールを設定する関数
  * @param userID ユーザーID
  * @param date 集中モードの終了時間
  */
-export function setSchedule(userID: string, date: Date) {
-  schedule.scheduleJob(userID, date, async () => {
-    await cronCheck();
+
+export function setSchedule(guild: Guild) {
+  schedule.scheduleJob("0 0 * * *", async () => {
+    try {
+      const imagePath = getRandomImage();
+      await guild.setIcon(imagePath, "Icon Botによる定時変更");
+      logger.info(`アイコンを変更しました:\n${imagePath}`);
+    } catch (error) {
+      logger.error("アイコンの変更に失敗しました");
+      logger.error(error);
+    }
   });
-  // logger.debug(`スケジュールを設定しました: ${userID}`);
-  // logger.debug(schedule.scheduledJobs);
 }
-
-/** スケジュールを削除する関数
- * @param userID ユーザーID
- */
-export function removeSchedule(userID: string) {
-  schedule.cancelJob(userID);
-  // logger.debug(`スケジュールを削除しました: ${userID}`);
-  // logger.debug(schedule.scheduledJobs);
-}
-
-process.on("SIGINT", () => {
-  schedule.gracefulShutdown().then(() => {
-    logger.info("停止します...");
-
-    process.exit(0);
-  });
-});
